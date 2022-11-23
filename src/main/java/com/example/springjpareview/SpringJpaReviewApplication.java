f3ba63fc559e37c17415b5ab638f2b27b9d36bc8
simhyunbo <hbo9347@naver.com>
2022-11-23 17:41:03 +0900

    Book,Author 테이블

---
+++ src/main/java/com/example/springjpareview/SpringJpaReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjpareview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaReviewApplication.class, args);
+    }
+
+}
